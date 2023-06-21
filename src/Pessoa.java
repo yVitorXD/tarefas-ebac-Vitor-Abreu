6e711837b6633b6c7f82ea0a30d1fa911cfaa3e1
yVitorXD <vitorxdxx@gmail.com>
2023-06-21 19:18:36 -0300

    Exercicio Módulo 7

---
+++ src/Pessoa.java
@@ -0,0 +1,38 @@
+
+// Classe pessoa, com suas caracteristicas e métodos acessores.
+public class Pessoa {
+
+	private String nome;
+	
+	private int idade;
+	
+	private Double altura;
+
+	
+	// Getters e Setters
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public Double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(Double altura) {
+		this.altura = altura;
+	}
+	
+	
+}
