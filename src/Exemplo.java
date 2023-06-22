b334c7471b73f0e17c43bdc736ad42482212d134
yVitorXD <vitorxdxx@gmail.com>
2023-06-22 14:34:04 -0300

    Exercicios Módulo 9

---
+++ src/Exemplo.java
@@ -0,0 +1,14 @@
+
+public class Exemplo {
+	public static void main(String[] args) {
+		
+		int idade = 10;
+		
+		System.out.println(idade);
+		
+		Integer idade2 = (Integer) idade;
+		
+		System.out.println(idade2);
+		
+	}
+}
